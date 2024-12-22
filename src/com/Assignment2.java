6363351d2c4f7f4b08aa31d5c0d7caa192859da0
vinod <vinodthatikonda15@gmail.com>
2024-12-22 18:37:57 +0530

    Assignment2 - Arrays

---
+++ src/com/Assignment2.java
@@ -0,0 +1,28 @@
+package com;
+
+public class Assignment2 {
+
+	
+
+	    public static void main(String[] args) {
+	        // Nested array to represent categories, subcategories, and products
+	        String[][][] supermarket = {
+	            // Category 1: Groceries
+	            {
+	                {"Cola", "Orange Juice", "Lemonade", "Green Tea", "Black Coffee "},      // Subcategory 1
+	                {"Popcorn", "Trail Mix", "Beef Jerky", "Salted Nuts", "Rice Cakes"},  // Subcategory 2
+	                {"Cottage Cheese", "Heavy Cream", "Ice Cream", "Parmesan Cheese", "Eggnog"}, // Subcategory 3
+	                {"Apples", "Bananas", "Carrots", "Potatoes", "Onions"} // Subcategory 4
+	            },
+	            // Category 2: Electronics
+	            {
+	                {"Laptop", "Tablet", "Smartphone", "Monitor", "Keyboard"},   // Subcategory 1
+	                {"Headphones", "Speakers", "Microphone", "Webcam", "Mouse"}, // Subcategory 2
+	                {"Television", "Projector", "Set-Top Box", "DVD Player", "Soundbar"}, // Subcategory 3
+	                {"Smartwatch", "Fitness Tracker", "Drone", "Camera", "Tripod"}       // Subcategory 4
+	            }
+	        };
+
+	        }
+	    }
+	
